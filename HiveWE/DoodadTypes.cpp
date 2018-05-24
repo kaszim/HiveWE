eacfe6f08570246ace6f0592afe2da9f96f6fb5b
Kasim Taskin <taskinkasim@gmail.com>
2018-05-24 23:14:06 +0200

    Use radians instead of degree

--- HiveWE/DoodadTypes.cpp
+++ HiveWE/DoodadTypes.cpp
@@ -8,6 +8,7 @@ DoodadTypes::DoodadTypes()
 
 void DoodadTypes::load() {
 	doodads_slk = slk::SLK("Doodads/Doodads.slk");
+	const auto deg2rad = glm::pi<float>() / 180.f;
 	for (auto y = 1; y < doodads_slk.rows; y++) {
 		auto row = doodads_slk.table_data[y];
 		types.emplace_back(DoodadType{
@@ -19,7 +20,7 @@ void DoodadTypes::load() {
 			std::stof(row[11]),
 			std::stof(row[12]),
 			std::stoi(row[20]),
-			std::stof(row[27]) == -1.f ? 0 : std::stof(row[27])
+			std::stof(row[27]) == -1.f ? 0 : std::stof(row[27]) * deg2rad
 			});
 		categories[types[y - 1].category].emplace_back(&types[y - 1]);
 	}
@@ -37,7 +38,7 @@ void DoodadTypes::load() {
 			std::stof(row[27]),
 			std::stof(row[28]),
 			std::stoi(row[21]),
-			std::stof(row[25]) == -1.f ? 0 : std::stof(row[25])
+			std::stof(row[25]) == -1.f ? 0 : std::stof(row[25]) * deg2rad
 			});
 		categories[types[y - 1].category].emplace_back(&types[y - 1]);
 	}
